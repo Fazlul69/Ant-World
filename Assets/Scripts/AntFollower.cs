54a862ff8bce4bd91e03748b42d46ad9dad01a76
Fazlul69 <sawravm299@gmail.com>
2019-12-26 12:57:41 +0600

    Initial commit

---
+++ Assets/Scripts/AntFollower.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class AntFollower : MonoBehaviour
+{
+    public PathCreator myPath;
+    float updatePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatePos += 5 * Time.deltaTime;
+        transform.position = myPath.path.GetPointAtDistance(updatePos);
+        Quaternion antRotation = myPath.path.GetRotationAtDistance(updatePos);
+
+        antRotation.x = 0;
+        antRotation.y = 0;
+        transform.rotation = antRotation;
+    }
+
+    public void GetPath(PathCreator newPath) {
+        myPath = newPath;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "antDetector") {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseDown() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform pathParent;
+    public PathCreator[] path;
+    [SerializeField] private Transform spawnPosition;
+    [SerializeField] private GameObject ant;
+    public AntFollower newAntFollower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //drug all of the path automatic,didnt need to drug the path
+        for (int i = 0; i < pathParent.transform.childCount; i++) {
+            path[i] = pathParent.transform.GetChild(i).GetComponent<PathCreator>();
+        }
+        StartCoroutine(spawnAnt());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnAnt() {
+        int randomPathIndex = Random.Range(0,3);
+
+        for (int i = 0; i < 5; i++) {
+
+            GameObject newAnt = Instantiate(ant, spawnPosition.position, Quaternion.identity) as GameObject;
+            newAnt.name = "Ant";
+            newAntFollower = newAnt.GetComponent<AntFollower>();
+            newAntFollower.GetPath(path[randomPathIndex]);
+
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(spawnAnt());
+    }
+}
